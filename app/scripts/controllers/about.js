b6455356b216212cbaeff72bbd23d64a1dce5b15
Constantin Lebrecht <constantin@some.local>
2016-04-15 10:32:31 +0200

    CREATE init import from yeoman generator

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name letItDropApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the letItDropApp
+ */
+angular.module('letItDropApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
