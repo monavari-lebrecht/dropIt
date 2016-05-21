fa6e723e2ab9e13b3ba6f90c3fd7e3ff5d3b0764
Constantin Lebrecht <constantin@some.speedport_w_724v_typ_a_05011603_00_009>
2016-05-21 20:32:23 +0200

    [UPDATE] ngApplication DropZone login/creation
    - add modal access to enter a drop zone key or to create a new on

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -1,19 +1,27 @@
 angular.module('letItDropApp').controller('LoginCtrl', ['$scope', '$http', '$cookies', function ($scope, $http, $cookies) {
   var _this = this;
 
+  $scope.dropZoneKey = '';
+  $scope.wrongKey = false;
+
   /**
    * function to create a new dropzone
    */
   $scope.create = function () {
     $http.get('api/dropZone/create').then(function (response) {
       $cookies.put('dropZoneKey', response.data.key);
+      $scope.$close();
     });
   };
 
   $scope.login = function () {
-    $http.get('api/dropZone/' + $scope.dropZoneKey + '/isValid').then(function () {
+    $http.get('api/dropZone/' + $scope.dropZoneKey + '/exists').then(function () {
       $cookies.put('dropZoneKey', $scope.dropZoneKey);
+      $scope.wrongKey = false;
+      $scope.$close();
     }).catch(function () {
+      // show hint about the wrong key
+      $scope.wrongKey = true;
       $cookies.remove('dropZoneKey');
     });
   }
